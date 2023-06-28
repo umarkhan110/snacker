1b9fab36dd4728e4ef725a07fd687e9e2d3d9d43
umarkhan110 <umardraz277@gmail.com>
2023-06-28 20:56:28 +0500

    style

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../index.css'
+import '../styles/globals.css'
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../config/firebase';
